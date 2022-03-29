1a10c2ea31eda0a9edfe15c2da1848a049161294
LaraGenovese <93852155+LaraGenovese@users.noreply.github.com>
2022-03-29 10:47:01 -0300

    Finished exercises 1 and 2

--- Assets/Script/Ejercicio1.cs
+++ Assets/Script/Ejercicio1.cs
@@ -7,7 +7,10 @@ public class Ejercicio1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Realizá un programa donde se cree una variable entera num1, que inicialmente valdrá 0. Luego incrementá su valor en 2 y mstrarlo por pantalla. Después mostrá el resultado de multiplicarlo por sí mismo.
+        int num1 = 0;
+        Debug.Log(num1 += 2);
+        Debug.Log(num1 * num1);
     }
 
     // Update is called once per frame
