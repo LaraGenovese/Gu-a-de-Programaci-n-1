f4a4889270e5530c9f9ec7810c8c15d31d8d2f0c
LaraGenovese <93852155+LaraGenovese@users.noreply.github.com>
2022-04-05 10:33:10 -0300

    Finished excercises 5 and 6

---
+++ Assets/Script/Ejercicio5.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio5 : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //5. Realizá un programa que, dados dos números enteros num1 y num2 cargados desde el Inspector muestre el resultado de la división del primero por el segundo en la consola siguiendo el formato num1 + “ / “ + num2 + “ = “ + resultado. En el caso de que el segundo valor num2 sea cero, mostrar el siguiente mensaje de error: “No se puede dividir por cero”.
+
+    public int num1;
+    public int num2;
+
+    void Start()
+    {
+        int resultado;
+
+        if(num2 != 0)
+        {
+            resultado = num1 / num2;
+            Debug.Log(num1 + "/" + num2 + " = " + resultado);
+        }
+        else
+        {
+            Debug.Log("No se puede dividir por cero");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/Ejercicio6.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio6 : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //Realizá un programa que permita el ingreso por Inspector de un valor entero mayor que 0 en una variable llamada num1 y muestre un mensaje por pantalla indicando &quot;el número es par&quot; o &quot;el número es impar&quot;. Deberá utilizar el operador “módulo” es el caracter %.
+
+    public int num1;
+
+    void Start()
+    {
+        if(num1 > 0)
+        {
+            if(num1 % 2 == 0)
+            {
+                Debug.Log("El número es par");
+            }
+            else
+            {
+                Debug.Log("El número es impar");
+            }
+        }
+        else
+        {
+            Debug.Log("El número ingresado debe ser mayor a cero");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
